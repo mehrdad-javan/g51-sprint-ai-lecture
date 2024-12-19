54580bc02506b66d7c45c15e6aaac4f23d59b0e1
Mehrdad Javan <mehrdad.javan@lexicon.se>
2024-12-19 10:44:31 +0100

    add flux reactive

--- src/main/java/se/lexicon/sprintailecture/controller/OpenAIChatController.java
+++ src/main/java/se/lexicon/sprintailecture/controller/OpenAIChatController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import se.lexicon.sprintailecture.service.OpenAIService;
 
 @RestController
@@ -17,4 +18,8 @@ public class OpenAIChatController {
         return  service.chat(question);
     }
 
+    @GetMapping("/reactive-chat")
+    public Flux<String> reactiveChat(@RequestParam String question){
+        return service.reactiveChat(question);
+    }
 }
--- src/main/java/se/lexicon/sprintailecture/service/OpenAIService.java
+++ src/main/java/se/lexicon/sprintailecture/service/OpenAIService.java
@@ -1,6 +1,11 @@
 package se.lexicon.sprintailecture.service;
 
+import reactor.core.publisher.Flux;
+
 public interface OpenAIService {
 
     String chat(String question);
+
+    Flux<String> reactiveChat(String question);
+
 }
